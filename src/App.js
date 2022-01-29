9f3df8e6a391891a785c126c179f62bd504888b3
Pankaj Prasun <pankaj1.prasun@ril.com>
2022-01-29 19:05:14 +0530

    Latest Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './component/Message';
 
 function App() {
   return (
@@ -7,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         ---Welcome Publishing the Articel-----
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> <Message/> </p>
       </header>
     </div>
   );
